8edcb226e1e4342bbd5da5cfb135bd235e656e92
cagrikarayaka <cagri.karayaka@gmail.com>
2023-01-03 00:46:16 +0300

    teste

---
+++ Core/NoteWith.Domain/EntitiyModels/BaseModels/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NoteWith.Domain.EntitiyModels.BaseModels
+{
+	public class BaseEntity
+	{
+		public BaseEntity()
+		{
+		}
+		public Guid ID { get; set; }
+
+		public Guid? Crea { get; set; }
+	}
+}
+
